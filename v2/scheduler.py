d45abbd556e1048da9325b8d26b93cc214712435
Guillaume Vincent <gvincent@redhat.com>
2019-04-17 15:10:11 +0200

    WIP New agent using docker container
    We need a scheduler to orchestrate the complexity in the agent.
    This scheduler is written in python and use ansible runner to manipulate DCI roles.
    
    Change-Id: I102376238acfe2d0cbf625e126f6c6e3e1470346

---
+++ v2/scheduler.py
@@ -0,0 +1,23 @@
+import os
+import ansible_runner
+
+from dciclient.v1.api.context import build_signature_context
+from dciclient.v1.api import remoteci
+from dciclient.v1.api import topic
+
+remoteci_id = os.getenv("DCI_CLIENT_ID").split("/")[1]
+context = build_signature_context()
+topic_id = topic.list(context, where="name:RHEL-7").json()["topics"][0]["id"]
+components_ids = context.session.post(
+    "%s/jobs/schedule" % context.dci_cs_api,
+    json={"topic_id": topic_id, "dry_run": True},
+).json()["components_ids"]
+ansible_runner.run(
+    private_data_dir="/opt/dci-rhel-agent",
+    playbook="agent.yml",
+    extravars={
+        "remoteci_id": remoteci_id,
+        "local_repo": "/var/www/html",
+        "components_ids": components_ids,
+    },
+)
