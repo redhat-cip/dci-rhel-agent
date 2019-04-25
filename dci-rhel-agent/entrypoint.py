ff2fb57ea6a3479548cff7da9d1daf91730172fc
Thomas Vassilian <tvassili@redhat.com>
2019-04-25 15:30:17 -0400

    [work in progress] Make dci-rhel-agent a podman container + multiserver
    Change-Id: I6ae2c9bdb589c658237ad31aaad3c8665b160813

---
+++ dci-rhel-agent/entrypoint.py
@@ -0,0 +1,26 @@
+import os
+import ansible_runner
+
+from dciclient.v1.api.context import build_signature_context
+from dciclient.v1.api import remoteci
+from dciclient.v1.api import topic
+
+topic = os.getenv('TOPIC')
+remoteci_id = os.getenv("DCI_CLIENT_ID").split("/")[1]
+context = build_signature_context()
+# topic_id = topic.list(context, where="name:%s" % topic).json()["topics"][0]["id"]
+# RHEL-7
+topic_id="e6ff2abf-b109-4dd5-b06a-39f8846ad252"
+components_ids = context.session.post(
+    "%s/jobs/schedule" % context.dci_cs_api,
+    json={"topic_id": topic_id, "dry_run": True},
+).json()["components_ids"]
+ansible_runner.run(
+    private_data_dir="/usr/share/dci-rhel-agent",
+    playbook="dci-downloader.yml",
+    extravars={
+        "remoteci_id": remoteci_id,
+        "local_repo": "/var/www/html",
+        "components_ids": components_ids,
+    },
+)
