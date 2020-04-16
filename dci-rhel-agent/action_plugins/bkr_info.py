c9a0ccac5fff3f8fe8468b05a2a8e10162cf6d73
Bill Peck <bill@pecknet.com>
2020-04-16 13:17:30 -0400

    Beaker feedback support
    While blocking on beaker system we now report the status
    every 10 seconds if -v is specified. Providing immediate
    feedback on the current status of the install.
    This will help identify stalled or broken installations.
    
    Change-Id: I8043346e54c717e9ec9d7737bc46c5900d73d85d

---
+++ dci-rhel-agent/action_plugins/bkr_info.py
@@ -0,0 +1,26 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.plugins.action import ActionBase
+from ansible.utils.display import Display
+
+import time
+
+display = Display()
+
+class ActionModule(ActionBase):
+
+    def run(self, tmp=None, task_vars=None):
+        super(ActionModule, self).run(tmp, task_vars)
+        module_args = self._task.args.copy()
+        module_return = dict(status="New")
+        while True:
+            module_return = self._execute_module(module_name='bkr_info',
+                                         module_args=module_args,
+                                         task_vars=task_vars, tmp=tmp)
+            display.v("BKR_INFO:%s" % module_return)
+            if (module_return["status"] in ["Completed", "Aborted"]):
+                break
+            time.sleep(10)
+
+        return module_return
