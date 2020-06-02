d7d092ac28555be8df03266f9a00eaccce131137
Michael Burke <miburke@redhat.com>
2020-06-02 14:03:46 -0400

    [WIP] Update the RHEL agent to remove the local beaker/ansible connection. Introduce dci-update.yml Create ssh keys if not present Update readme with manual steps to copy keys to applicable systems. Add beaker-repo-update task
    Change-Id: Id624b1cacc685514847487027b1577fdb7839e24

--- dci-rhel-agent/entrypoint.py
+++ dci-rhel-agent/entrypoint.py
@@ -60,7 +60,7 @@ def load_settings():
 
 
 def provision_and_test(extravars):
-    # # Path is static in the container
+    # Path is static in the container
     # local_repo = '/var/www/html'
     # extravars['local_repo'] = local_repo
 
@@ -76,6 +76,7 @@ def provision_and_test(extravars):
             print ('The dci-rhel-agent is configured in download-only mode.')
             sys.exit(0)
 
+    # Download and import Compose
     r = ansible_runner.run(
         private_data_dir="/usr/share/dci-rhel-agent/",
         inventory="/etc/dci-rhel-agent/inventory",
@@ -88,6 +89,7 @@ def provision_and_test(extravars):
         print ("Distro(s) import in Beaker has failed. {}: {}".format(r.status, r.rc))
         sys.exit(1)
 
+    # Provision and install SUT
     if 'systems' not in extravars.keys():
         print ('No hosts found in settings. You should configure download-only mode or add systems[].')
         sys.exit(1)
@@ -124,6 +126,19 @@ def main():
     if environ.get('DCI_CLIENT_ID') is None:
         print ("Environment variable DCI_CLIENT_ID not set.")
         sys.exit(1)
+
+    # Run the update playbook once before jobs.
+    r = ansible_runner.run(
+        private_data_dir="/usr/share/dci-rhel-agent/",
+        inventory="/etc/dci-rhel-agent/inventory",
+        verbosity=1,
+        playbook="dci-update.yml",
+        quiet=False
+    )
+    if r.rc != 0:
+        print ("Update playbook failed. {}: {}".format(r.status, r.rc))
+        sys.exit(1)
+
     # Read the settings file
     sets = load_settings()
     # Check if the settings contain multiple topics and process accordingly
